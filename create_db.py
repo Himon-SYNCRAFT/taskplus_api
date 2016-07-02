3efae3fdf536580ad1918b0702602bccfe58c7be
Himon-SYNCRAFT <d.zawlocki@syncraft.pl>
2016-07-02 22:44:13 +0200

    New models. create_db function create sample data in database.

--- create_db.py
+++ create_db.py
@@ -1,11 +1,83 @@
 from database import db_session, Model
 import models
 
+
 def create_db():
 
     Model.metadata.reflect()
     Model.metadata.drop_all()
     Model.metadata.create_all()
+
+    statuses = [
+        models.TaskStatus(name='Nowe'),
+        models.TaskStatus(name='W trakcie realizacji'),
+        models.TaskStatus(name='Zrealizowane'),
+        models.TaskStatus(name='Anulowane')
+    ]
+
+    for item in statuses:
+        db_session.add(item)
+
+    db_session.commit()
+
+    task_types = [
+        models.TaskType(name='Zmiana ceny'),
+        models.TaskType(name='Dodaj nowy produkt'),
+    ]
+
+    for item in task_types:
+        db_session.add(item)
+
+    db_session.commit()
+
+    users = [
+        models.User('admin', 'admin', 'Daniel', 'Zawłocki', True, True, True),
+        models.User('danzaw', 'danzaw', 'Daniel', 'Zawłocki', True, True, False),
+        models.User('przoci', 'przoci', 'Przemek', 'Ociepa', False, True, False)
+    ]
+
+    for item in users:
+        db_session.add(item)
+
+    db_session.commit()
+
+    task_attribute_types = [
+        models.TaskAttributeType(name='string'),
+        models.TaskAttributeType(name='int'),
+        models.TaskAttributeType(name='float'),
+        models.TaskAttributeType(name='list'),
+        models.TaskAttributeType(name='json'),
+    ]
+
+    for item in task_attribute_types:
+        db_session.add(item)
+
+    db_session.commit()
+
+    task_attributes = [
+        models.TaskAttribute(name='Nazwa produktu', type_id=1),
+        models.TaskAttribute(name='Cena', type_id=3),
+        models.TaskAttribute(name='Opis', type_id=1),
+    ]
+
+    for item in task_attributes:
+        db_session.add(item)
+
+    db_session.commit()
+
+    task_attribute_to_task_types = [
+        models.TaskAttributeToTaskType(1, 1),
+        models.TaskAttributeToTaskType(2, 1),
+        models.TaskAttributeToTaskType(1, 2),
+        models.TaskAttributeToTaskType(2, 2),
+        models.TaskAttributeToTaskType(2, 3)
+    ]
+
+    for item in task_attribute_to_task_types:
+        db_session.add(item)
+
+    db_session.commit()
+
 
 if __name__ == '__main__':
     create_db()
--- models.py
+++ models.py
@@ -1,16 +1,47 @@
+from datetime import datetime
 from database import Model
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text, \
+    Boolean, LargeBinary
 from sqlalchemy.orm import relationship
+from flask_bcrypt import Bcrypt
+from endpoints import app
+
+bcrypt = Bcrypt(app)
 
 
 class User(Model):
     __tablename__ = 'users'
 
     id = Column(Integer(), primary_key=True)
-    name = Column(String(length=128), unique=True, nullable=False)
+    login = Column(String(length=128), unique=True, nullable=False)
+    first_name = Column(String(length=128), nullable=False)
+    last_name = Column(String(length=128), nullable=False)
+    password = Column(LargeBinary(60), nullable=False)
+    is_creator = Column(Boolean(), nullable=False, default=False)
+    is_contractor = Column(Boolean(), nullable=False, default=False)
+    is_admin = Column(Boolean(), nullable=False, default=False)
 
-    created_tasks = relationship('Task', back_populates='creator')
-    completed_tasks = relationship('Task', back_populates='contractor')
+    created_tasks = relationship(
+        'Task', back_populates='creator', foreign_keys='Task.creator_id')
+    completed_tasks = relationship(
+        'Task', back_populates='contractor', foreign_keys='Task.contractor_id')
+
+    def __init__(self, login, password, first_name, last_name,
+                 is_creator=False, is_contractor=False, is_admin=False):
+        self.login = login
+        self.first_name = first_name
+        self.last_name = last_name
+        self.is_creator = is_creator
+        self.is_contractor = is_contractor
+        self.is_admin = is_admin
+
+        self.generate_password_hash(password)
+
+    def generate_password_hash(self, password):
+        self.password = bcrypt.generate_password_hash(password, rounds=12)
+
+    def check_password_hash(self, password):
+        return bcrypt.check_password_hash(self.password, password)
 
 
 class Task(Model):
@@ -18,21 +49,32 @@ class Task(Model):
 
     id = Column(Integer(), primary_key=True)
     name = Column(String(length=128), nullable=False)
-    external_identifier = Column(String(length=128))
+    external_identifier = Column(String(length=128), nullable=True)
     type_id = Column(Integer(), ForeignKey('task_types.id'), nullable=False)
+    end_date = Column(DateTime())
+    create_date = Column(DateTime(), nullable=False)
+
+    # Foreign keys
     status_id = Column(Integer(), ForeignKey(
         'task_statuses.id'), nullable=False)
     creator_id = Column(Integer(), ForeignKey('users.id'), nullable=False)
     contractor_id = Column(Integer(), ForeignKey('users.id'), nullable=True)
-    create_date = Column(DateTime(), nullable=False)
-    end_date = Column(DateTime())
 
+    # Relationships
     status = relationship('TaskStatus', back_populates='tasks')
     type = relationship('TaskType', back_populates='tasks')
     creator = relationship(
-        'User', back_populates='created_tasks', foreign_keys='creator_id')
+        'User', back_populates='created_tasks', foreign_keys=[creator_id])
     contractor = relationship(
-        'User', back_populates='completed_tasks', foreign_keys='contractor_id')
+        'User', back_populates='completed_tasks', foreign_keys=[contractor_id])
+
+    def __init__(self, name, type_id, status_id, creator_id, external_identifier=None):
+        self.name = name
+        self.type_id = type_id
+        self.status_id = status_id
+        self.creator_id = creator_id
+        self.external_identifier = external_identifier
+        self.create_date = datetime.today()
 
 
 class TaskStatus(Model):
@@ -43,6 +85,9 @@ class TaskStatus(Model):
 
     tasks = relationship('Task', back_populates='status')
 
+    def __init__(self, name):
+        self.name = name
+
 
 class TaskType(Model):
     __tablename__ = 'task_types'
@@ -50,7 +95,13 @@ class TaskType(Model):
     id = Column(Integer(), primary_key=True)
     name = Column(String(length=128), unique=True, nullable=False)
 
+    # Relationships
     tasks = relationship('Task', back_populates='type')
+    attributes = relationship(
+        'TaskAttribute', secondary='task_attribute_to_task_type')
+
+    def __init__(self, name):
+        self.name = name
 
 
 class TaskAttribute(Model):
@@ -58,8 +109,31 @@ class TaskAttribute(Model):
 
     id = Column(Integer(), primary_key=True)
     name = Column(String(length=128), unique=True, nullable=False)
+
+    # Foreign keys
     type_id = Column(Integer(), ForeignKey(
         'task_attribute_types.id'), nullable=False)
+
+    def __init__(self, name, type_id):
+        self.name = name
+        self.type_id = type_id
+
+
+class TaskAttributeToTaskType(Model):
+    __tablename__ = 'task_attribute_to_task_type'
+
+    task_type_id = Column(Integer(), ForeignKey(
+        'task_types.id'), primary_key=True)
+    task_attribute_id = Column(Integer(), ForeignKey(
+        'task_attributes.id'), primary_key=True)
+    sort = Column(Integer(), nullable=False, default=0)
+    rules = Column(Text)
+
+    def __init__(self, task_type_id, task_attribute_id, sort=0, rules=None):
+        self.task_type_id = task_type_id
+        self.task_attribute_id = task_attribute_id
+        self.sort = sort
+        self.rules = rules
 
 
 class TaskAttributeValue(Model):
@@ -68,9 +142,15 @@ class TaskAttributeValue(Model):
     id = Column(Integer(), primary_key=True)
     value = Column(Text(), nullable=False)
 
+    def __init__(self, value):
+        self.value = value
+
 
 class TaskAttributeType(Model):
-    __tablename__ = 'task_attribute_type'
+    __tablename__ = 'task_attribute_types'
 
     id = Column(Integer(), primary_key=True)
     name = Column(String(length=128), unique=True, nullable=False)
+
+    def __init__(self, name):
+        self.name = name
