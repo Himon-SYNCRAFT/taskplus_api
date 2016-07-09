59a84b4f9f60412bd26e049ce3521e3b7001a7e8
Himon-SYNCRAFT <d.zawlocki@syncraft.pl>
2016-07-09 14:24:06 +0200

    Switching on foreing keys in sqlite

--- create_db.py
+++ create_db.py
@@ -1,3 +1,5 @@
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 from database import Model
 import models
 
@@ -6,6 +8,13 @@ def create_db(db_session=None):
 
     if db_session is None:
         from database import db_session
+
+    if db_session.bind.driver == 'pysqlite':
+        @event.listens_for(Engine, "connect")
+        def set_sqlite_pragma(dbapi_connection, connection_record):
+            cursor = dbapi_connection.cursor()
+            cursor.execute("PRAGMA foreign_keys=ON")
+            cursor.close()
 
     Model.metadata.reflect(db_session.bind)
     Model.metadata.drop_all(db_session.bind)
